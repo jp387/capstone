471cc0dbe46e574fb131f3ad27c7e8b021b3b0a6
John Phung <141362328+jp387@users.noreply.github.com>
2023-11-12 15:10:41 -0800

    Initial commit

---
+++ WhatsForDinner/WhatsForDinner/WhatsForDinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsForDinnerApp.swift
+//  WhatsForDinner
+//
+//  Created by John Phung on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WhatsForDinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
